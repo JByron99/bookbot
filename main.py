384e9a216f8c04ac12690499e7dd2b955514efae
JByron99 <sbyron@pm.me>
2024-10-08 01:33:18 -0400

    completed lesson 11

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+def main():
+    book_source = "books/frankenstein.txt"
+    with open(book_source) as f:
+        open_book = f.read()
+        f.close()
+
+        # print_book_to_console(book)
+        word_count = count_words(open_book)
+        letter_count = count_letters(open_book)
+
+        sorted_letter_list = dict_to_sorted_dict(letter_count)
+
+        print(f"--- Begin report of {book_source} ---")
+        print(f"{word_count} words found in the document.")
+        print("")
+        for letter in sorted_letter_list:
+            print(f"The letter {letter} was found {sorted_letter_list[letter]} times.")
+        print("--- End report ---")
+
 def print_book_to_console(book):
     print(book)
+
 
 def count_words(book):
     words = book.split()
     return len(words)
+
 
 def count_letters(book):
     all_lowercase = book.lower()
@@ -16,13 +37,25 @@ def count_letters(book):
         if character in alphabet_count:
             alphabet_count[character] += 1
     
-    print(alphabet_count)
-    return 0
+    return alphabet_count
 
-if __name__ == "__main__":
-    with open("books/frankenstein.txt") as f:
-        book = f.read()
-        f.close()
-        # print_book_to_console(book)
-        # print(count_words(book))
-        count_letters(book)
+
+def get_value_hack(given_dict):
+    # Returns the first value given from a dictionary
+    for i in given_dict:
+        return given_dict[i]
+    
+
+
+def dict_to_sorted_dict(my_dict):
+    my_list = []
+    sorted_dict = {}
+    for i in my_dict:
+        my_list.append({i:my_dict[i]})
+    my_list.sort(reverse=True, key=get_value_hack)
+    for dict in my_list:
+        sorted_dict.update(dict)
+
+    return sorted_dict
+
+main()
